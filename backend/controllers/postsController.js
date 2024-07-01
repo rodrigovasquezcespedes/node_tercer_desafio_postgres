56f4e8aa5e0367f8bd55305a163d77df68480546
rodrigo vasquez <rvasquez26@gmail.com>
2024-06-30 22:25:40 -0400

    instalacion de standard

--- backend/controllers/postsController.js
+++ backend/controllers/postsController.js
@@ -1,40 +1,26 @@
-const { pool } = require("../config/db");
+const { pool } = require('../config/db')
 
 const getPost = async () => {
   try {
-    const query = "SELECT * FROM posts;";
-    const { rows } = await pool.query(query);
-    return rows;
+    const query = 'SELECT * FROM posts'
+    const { rows } = await pool.query(query)
+    return rows
   } catch (error) {
-    console.error(`Error al obtener los posts: ${error.message}`);
-    throw new Error(`Error al obtener los posts: ${error.message}`);
+    console.error(`Error al obtener los posts: ${error.message}`)
+    throw new Error(`Error al obtener los posts: ${error.message}`)
   }
-};
+}
 
 const createPost = async ({ titulo, url, descripcion, likes = 0 }) => {
   try {
-    const query = "INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING *";
-    const values = [titulo, url, descripcion, likes];
-    const { rows } = await pool.query(query, values);
-    return rows;
+    const query = 'INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING *'
+    const values = [titulo, url, descripcion, likes]
+    const { rows } = await pool.query(query, values)
+    return rows
   } catch (error) {
-    console.error(`Error al guardar el post: ${error.message}`);
-    throw new Error(`Error al guardar el post: ${error.message}`);
+    console.error(`Error al guardar el post: ${error.message}`)
+    throw new Error(`Error al guardar el post: ${error.message}`)
   }
-};
+}
 
-/*
-const deletePost = async (postId) => {
-  try {
-    const query = "DELETE FROM posts WHERE id = $1 RETURNING *";
-    const values = [postId];
-    const { rows } = await pool.query(query, values);
-    return rows;
-  } catch (error) {
-    console.error(`Error al eliminar el post: ${error.message}`);
-    throw new Error(`Error al eliminar el post: ${error.message}`);
-  }
-};
-*/
-module.exports = { getPost, createPost/*, deletePost*/ };
-
+module.exports = { getPost, createPost }
--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,31 @@
-const express = require("express");
-const cors = require("cors");
-const { getPost, createPost } = require("./controllers/postsController");
-const app = express();
-const port = 3000;
-app.use(cors());
-app.use(express.json());
+const express = require('express')
+const cors = require('cors')
+const { getPost, createPost } = require('./controllers/postsController')
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.status(200).send("Api Likeme");
-});
+app.get('/', (req, res) => {
+  res.status(200).send('Bienvenido a mi aplicación')
+})
 
-app.get("/posts", async (req, res) => {
+app.get('/posts', async (req, res) => {
   try {
-    const posts = await getPost();
-    res.status(200).send(posts);
+    const posts = await getPost()
+    res.status(200).send(posts)
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send(error)
   }
-});
+})
 
-app.post("/posts", async (req, res) => {
+app.post('/posts', async (req, res) => {
   try {
-    await createPost(req.body);
-    res.status(201).send("Post creado con exito");
+    await createPost(req.body)
+    res.status(201).send('Post creado con exito')
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send(error)
   }
-});
+})
 
-/*
-app.delete("/posts/:id", async (req, res) => {
-  const postId = req.params.id;
-  try {
-    const deletedPost = await deletePost(postId);
-    res.status(200).send(`Post con ID ${postId} eliminado correctamente`);
-  } catch (error) {
-    res.status(400).send(`Error al eliminar el post con ID ${postId}: ${error.message}`);
-  }
-});
-
-app.put("/posts/:id", async (req, res) => {
-  const postId = req.params.id;
-  const { titulo, url, descripcion, likes } = req.body;
-  try {
-    await updatePost(postId, { titulo, url, descripcion, likes });
-    res.status(200).send(`Post con ID ${postId} actualizado correctamente`);
-  } catch (error) {
-    res.status(400).send(`Error al actualizar el post con ID ${postId}: ${error.message}`);
-  }
-});
-
-*/
-app.listen(port, () => console.log(`Escuchando en el puerto ${port}`));
+app.listen(port, () => console.log(`Escuchando en el puerto ${port}`))
